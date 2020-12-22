e7a4ca658a71d476354e59a7bb9982984aa5d26d
Jason Huang <jasonhuang20035@gmail.com>
2020-12-22 13:06:04 -0500

    feat: add qualifications function
    querying the entire doc has been implemented

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,8 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions")
+const admin = require("firebase-admin")
+admin.initializeApp()
+
+const db = admin.firestore()
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +11,17 @@ const functions = require('firebase-functions');
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+exports.getQualifications = functions.https.onCall(async (data, context) => {
+  const qualRef = db.collection("qualifications")
+  if (data) {
+    // do searching
+  } else {
+    const querySnapshot = await qualRef.get()
+    const formattedObject = {}
+    querySnapshot.forEach(documentSnapshot => {
+      formattedObject[documentSnapshot.id] = documentSnapshot.data()
+    })
+    return formattedObject
+  }
+})
