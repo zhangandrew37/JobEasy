969f86d63ccaaf75b72eab6d0ee309ff3c072e83
Jason Huang <jasonhuang20035@gmail.com>
2021-01-02 16:54:21 -0500

    chore: delete unneeded comment

--- functions/index.js
+++ functions/index.js
@@ -7,14 +7,6 @@ admin.initializeApp()
 const firestore = admin.firestore()
 const GeoFirestore = geofirestore.initializeApp(firestore)
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
 exports.getQualifications = functions.https.onCall(async (data, context) => {
   const qualRef = firestore.collection("qualifications")
   const querySnapshot = await qualRef.get()
