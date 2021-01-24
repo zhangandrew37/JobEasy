9b0bf479ae0b89884b9ca1dd11fa2e700ce7b9fd
FranklinGao123 <51179937+FranklinGao123@users.noreply.github.com>
2021-01-23 19:35:13 -0500

    Update 1/23/2021

--- src/components/hero.js
+++ src/components/hero.js
@@ -2,43 +2,50 @@ import React from "react"
 
 import { Box, Heading, SimpleGrid, Image, Flex, Button } from "@chakra-ui/react"
 import { FaArrowRight } from "react-icons/fa"
+import { Link, BrowserRouter as Router } from "react-router-dom"
 
 import HeroImage from "../images/undraw_Co_workers_re_1i6i.svg"
+import SecondPage from "./pages/SecondPage"
 
 const Hero = () => (
   <Box>
-    <Heading size="4xl" mb={10}>
-      Put some hero text here
-    </Heading>
-    <Flex
-      direction="row"
-      wrap={{ base: "wrap", md: "nowrap" }}
-      justify="center"
-      pb={10}
-    >
-      <Box>
-        <Heading size="xl" mb={10}>
-          A simple job finder for those who need it most
-        </Heading>
-        <Button size="lg" rightIcon={<FaArrowRight />} colorScheme="blue">
-          Get Started
-        </Button>
-      </Box>
-      <Image h="auto" flexBasis={{ base: "sm", md: "xl" }} as={HeroImage} />
-      {/* <Image
+    <Router>
+      <Heading size="4xl" mb={10}>
+        Job Finding as Easy as a Click
+      </Heading>
+      <Flex
+        direction="row"
+        wrap={{ base: "wrap", md: "nowrap" }}
+        justify="center"
+        pb={10}
+      >
+        <Box>
+          <Heading size="xl" mb={10}>
+            A simple job finder for those who need it most
+          </Heading>
+
+          <Link to="/SecondPage">
+            <Button size="lg" rightIcon={<FaArrowRight />} colorScheme="blue">
+              Get Started
+            </Button>
+          </Link>
+        </Box>
+        <Image h="auto" flexBasis={{ base: "sm", md: "xl" }} as={HeroImage} />
+        {/* <Image
         as={HeroImage}
         objectFit="fill"
         p={{ base: 10, md: 0 }}
         w={{ base: "lg", md: "xl" }}
       /> */}
-    </Flex>
+      </Flex>
 
-    <SimpleGrid columns={2} spacing={6}>
-      <Box h="200px" bg="gray.50" rounded="md" boxShadow="md"></Box>
-      <Box h="200px" bg="gray.50" rounded="md" boxShadow="md"></Box>
-      <Box h="200px" bg="gray.50" rounded="md" boxShadow="md"></Box>
-      <Box h="200px" bg="gray.50" rounded="md" boxShadow="md"></Box>
-    </SimpleGrid>
+      <SimpleGrid columns={2} spacing={6}>
+        <Box h="200px" bg="gray.50" rounded="md" boxShadow="md"></Box>
+        <Box h="200px" bg="gray.50" rounded="md" boxShadow="md"></Box>
+        <Box h="200px" bg="gray.50" rounded="md" boxShadow="md"></Box>
+        <Box h="200px" bg="gray.50" rounded="md" boxShadow="md"></Box>
+      </SimpleGrid>
+    </Router>
   </Box>
 )
 
